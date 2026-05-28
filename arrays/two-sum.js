bc3616b8f3df16796af6cb1935b76b3d1a7a0ca9
maurirod48 <maurirodriguezm48@gmail.com>
2026-05-28 14:40:17 -0600

    Change (two-sum.js)
    I moved two-sum.js to ~/Desktop/LeetCode/arrays.

--- arrays/two-sum.js
+++ arrays/two-sum.js
@@ -1,10 +1,42 @@
 /**
- * Using a hash map to solve twoSum leetcode problem.
+ * ## Problem: [Two Sum] (LC #1).
+ * 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ * 
+ * ## Link: https://leetcode.com/problems/two-sum
+ * ## Difficulty: Easy
+ * ## Pattern: Hash Map.
+ *
+ * ## Approach:
+ * 
+ * We are given a target (integer) and an array of integers. We have to find 2 numbers in the array that add up to the target, 
+ * but the same array element cannot be used twice. First I'd create a hash map, then use for loop to iterate through each element. For each iteration 
+ * we do this: 
+ * 
+ * - Calculate the difference between the target and the current element and call it "complement".
+ * - Check if "complement" exists in hash map. 
+ * - If "complement" does not exist in hash map, we add the current array element to the hash map (key: element, value: element index).
+ * - If "complement" does exist in hash map, it means we've found our answer so we just return the current element index
+ *   and the key-value entry whose key is equal to "complement" which stores the other element index.
+ * 
+ * ## Time Complexity:  O(n)
+ * ## Space Complexity: O(?)
+ *
+ * ## Mistakes / Things I learned:
+ *   At this point I'm learning how to use hash maps to resolve DSA problems. the first mistake I was was use 2 for loops to 
+ *   iterate thru the array twice making O(n^2). I wouldn't call it a mistake really, I just learned that using a hash map would 
+ *   reduce the runtime. Using a hash map helps reduce the runtime to O(n) because we only iterate thru the array once and avoids
+ *   the need of using nested loops.
  */
 
 
 // Array.
-const nums = [3, 3];
+const nums1 = [3, 3];
+const nums2 = [3, 1, 2, 5, 9];
 
 // Target.
 const target = 6;
@@ -39,4 +71,4 @@ var twoSum = function(nums, target) {
 }
 
 
-console.log(twoSum(nums, target));
+console.log(twoSum(nums2, target));
